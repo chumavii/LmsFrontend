2f41b60e0e30b6d36b647ea0e30bf8579766dc3b
Chuma <chuxnwuba@gmail.com>
2025-07-09 23:23:22 -0400

    updated api urls

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -18,7 +18,7 @@ function LoginForm({ onLoginSuccess }: LoginFormProps) {
     setError('');
 
     try {
-      const response = await fetch(`${BASE_URL}/login`, {
+      const response = await fetch(`${BASE_URL}/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
